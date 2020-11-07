f9fb5a77f3f78f10c66ee5e54d96494488f01a7c
dibya <dibyajyotidas000@gmail.com>
2020-11-08 01:25:35 +0530

    added Search_Customer file and modified TC-004 file

---
+++ src/test/java/learn/automation/Search_Customer.java
@@ -0,0 +1,7 @@
+package learn.automation;
+
+public class Search_Customer {
+//This is my search customer 
+	
+	//This is code added newly
+}
--- src/test/java/learn/automation/TC-004.java
+++ src/test/java/learn/automation/TC-004.java
@@ -1,2 +1,9 @@
 //This is a new test case.
 //This is added by someone else
+//this code added newly
+//this code added newly
+//this code added newly
+//this code added newly
+
+
+
